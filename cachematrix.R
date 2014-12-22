b9190c7aa3ac2eb9552c6e07bafdf5a2714fb175
Luis <l.s.mino@gmail.com>
2014-12-22 01:08:07 +0100

    Calculation of inverse
    Calculation and cache of inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Essentially the first function define three functions that are later used
+## to: 1. recover the cached data, 2. recover the cached inverse, or 3. cache
+## the inverse. Then, function cacheSolve first checks if the inverse has been
+## already cached, and if not, recover the data, compute the inverse and store
+## it in cache.
 
-## Write a short comment describing this function
+## Definition of the three functions used
 
 makeCacheMatrix <- function(x = matrix()) {
+	
+		inverseMat <- NULL
+
+        getCache <- function() x
+        setInv <- function(invMat) inverseMat <<- invMat
+        getInv <- function() inverseMat
+        list(getCache = getCache,
+             setInv = setInv,
+             getInv = getInv)
+	        
 
 }
 
 
-## Write a short comment describing this function
+## Calculation of the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	
+       ## Return a matrix that is the inverse of 'x'
+        
+        inverseMat <- x$getInv()
+        if(!is.null(inverseMat)) {
+                message("getting cached data")
+                return(inverseMat)
+        }
+        dataMat <- x$getCache()
+        inverseMat <- solve(dataMat)
+        x$setmean(inverseMat)
+        inverseMat
+        
 }
